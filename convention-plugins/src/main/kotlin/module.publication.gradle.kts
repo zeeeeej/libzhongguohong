2342516d30fafbd7766c5287cbfb61338c65e707
xiangpl <xiangpengle@qq.com>
2024-05-28 13:59:07 +0800

    init

---
+++ convention-plugins/src/main/kotlin/module.publication.gradle.kts
@@ -0,0 +1,51 @@
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.kotlin.dsl.`maven-publish`
+
+plugins {
+    `maven-publish`
+    signing
+}
+
+publishing {
+    // Configure all publications
+    publications.withType<MavenPublication> {
+        // Stub javadoc.jar artifact
+        artifact(tasks.register("${name}JavadocJar", Jar::class) {
+            archiveClassifier.set("javadoc")
+            archiveAppendix.set(this@withType.name)
+        })
+
+        // Provide artifacts information required by Maven Central
+        pom {
+            name.set("Kotlin Multiplatform Zhong Guo Hong")
+            description.set(" Zhong Guo Hong")
+            url.set("https://github.com/zeeeeej/libzhongguohong")
+
+            licenses {
+                license {
+                    name.set("MIT")
+                    url.set("https://opensource.org/licenses/MIT")
+                }
+            }
+            developers {
+                developer {
+                    id.set("zeeeeej.xpl")
+                    name.set("zeeeeej")
+                    organization.set("zeeeeej.xpl")
+                    organizationUrl.set("https://github.com/zeeeeej")
+                }
+            }
+            scm {
+                url.set("https://github.com/zeeeeej/libzhongguohong")
+            }
+        }
+    }
+}
+
+signing {
+    if (project.hasProperty("signing.gnupg.keyName")) {
+        useGpgCmd()
+        sign(publishing.publications)
+    }
+}
